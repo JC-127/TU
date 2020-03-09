0fce0bd02864a267aabd49c3182fa90ea1578942
Juan-Carlo V. Mercado <59926633+JC-127@users.noreply.github.com>
2020-03-09 13:02:28 -0400

    Update myShellHeader.h
    Changed the Variable names to make it a bit more clear to me, i got confused in a process.

--- myShell/myShellHeader.h
+++ myShell/myShellHeader.h
@@ -4,13 +4,13 @@
 //prototypes for the functions that will be in the main
 //if i do separate c files for all of these, it will be too many
 int IfFileIsBatch(char* stringFile, int position);
-void BatchExecute(int position, char** envp, char* argv[], char* stringFile);
-void ReadingCommand(int argc, char** envp, char* argv[], char* stringFile);
-void InternalCommand(int command, char* stringFuke, char** envp);
+void BatchExecute(int position, char** condition2, char* condition1[], char* stringFile);
+void ReadingCommand(int argc, char** condition2, char* condition1[], char* stringFile);
+void InternalCommand(int command, char* stringFile, char** condition2);
 void CurrentDirectoryCommand(char* stringFile); //this is dir
 void ChangeDirectoryCommand(char* stringFile); //this is cd
 void HelpCommand(char* stringFile); //this is help 
-int isInCommand(char* stringFile);
+int IfCurrentlyInCommand(char* stringFile);
 
 enum Commands
 {
