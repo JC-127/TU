d00859be0591e51c694b339f4bc21ecb925bef2e
Juan-Carlo V. Mercado <59926633+JC-127@users.noreply.github.com>
2020-03-09 12:54:04 -0400

    Add files via upload
    Shell .c Files of what I have so far. This will be done before due date.

---
+++ myShell/ShellMain.c
@@ -0,0 +1,41 @@
+//Program will simulate a linux shell.
+
+//INCLUSIIONS
+#include <stdio.h>
+#include <stdlib.h>
+
+//these two probably wont be necessary, but i added them just in case
+#include <string.h>
+#include <ctype.h>
+
+//Header Inclusion
+#include "myShellHeader.h"
+
+//MAIN 
+//these are not really conditions,
+//its really arguements but the term arg seemed redundant and overused
+int main(int stringFile, char *condition1, char **condition2)
+{
+	//reminder for myself, in C 0 is false and 1 is true
+	int WaitingForCommand = 1; //this has to be true at first
+
+	char string1[150] = ""; //Empty character array initially
+	
+
+	while (WaitingForCommand) 
+	{
+		if ( IfFileIsBatch(string1, stringFile) )  
+		{
+			//if file is a batch then a function called "BatchExecute" will be called
+			BatchExecute(stringFile, condition1, condition2, string1);
+			exit(0);
+		}//end if()
+
+		ReadingCommand(stringFile, condition1, condition2, string1);
+
+	}//end while
+	
+}//end main()
+
+
+
