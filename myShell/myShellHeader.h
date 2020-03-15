5b0fea01d3bf6d72519b585415f8997e49ee656a
Juan-Carlo V. Mercado <59926633+JC-127@users.noreply.github.com>
2020-03-15 15:59:09 -0400

    Update myShellHeader.h
    Had to name unique variables, lotta cross overs due to similar names

--- myShell/myShellHeader.h
+++ myShell/myShellHeader.h
@@ -3,14 +3,14 @@
 
 //prototypes for the functions that will be in the main
 //if i do separate c files for all of these, it will be too many
-int IfFileIsBatch(char* stringFile, int position);
-void BatchExecute(int position, char* condition1[], char** condition2, char* stringFile);
-void ReadingCommand(int position, char* condition1[], char** condition2, char* stringFile);
-void InternalCommand(int command, char* stringFile, char** condition2);
-void CurrentDirectoryCommand(char* stringFile); //this is dir
-void ChangeDirectoryCommand(char* stringFile); //this is cd
-void HelpCommand(char* stringFile); //this is help 
-int IfCurrentlyInCommand(char* stringFile);
+int IfFileIsBatch(char* stringBatchFile, int BatchPosition);
+void BatchExecute(int BEPosition, char* BECondition1[], char** BECondition2, char* BEstringFile);
+void ReadingCommand(int ReadPosition, char* ReadCondition1[], char** ReadCondition2, char* ReadStringFile);
+void InternalCommand(int ICPosition, char* ICStringFile, char** ICCondition);
+void CurrentDirectoryCommand(char* CDir); //this is dir
+void ChangeDirectoryCommand(char* CDStringFile); //this is cd
+void HelpCommand(char* HelpStringFile); //this is help 
+int IfCurrentlyInCommand(char* IFCICstringFile);
 
 enum Commands
 {
